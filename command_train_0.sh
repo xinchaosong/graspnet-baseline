a48e0f8bce9b7d0ffc3bb816bf77967700abe6ce
Xinchao Song <git@xinchaosong.com>
2022-05-23 21:26:38 -0400

    Change

--- command_train_0.sh
+++ command_train_0.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --camera realsense --log_dir logs/log_rs_c --dataset_root /media/DATACENTER2/HiLo_other_approaches/GraspNet_1B --batch_size 6
+CUDA_VISIBLE_DEVICES=0 python train.py --camera realsense --log_dir logs/log_rs_c --dataset_root /media/DATACENTER2/HiLo_other_approaches/GraspNet_1B --batch_size 4
