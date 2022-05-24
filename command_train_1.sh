1185c903a495e8683b6c64551211c005a1a512ab
Xinchao Song <git@xinchaosong.com>
2022-05-23 21:20:00 -0400

    Change

--- command_train_1.sh
+++ command_train_1.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python train.py --camera kinect --log_dir logs/log_kn_c --dataset_root /media/DATACENTER2/HiLo_other_approaches/GraspNet_1B --batch_size 8
+CUDA_VISIBLE_DEVICES=1 python train.py --camera kinect --log_dir logs/log_kn_c --dataset_root /media/DATACENTER2/HiLo_other_approaches/GraspNet_1B --batch_size 6
